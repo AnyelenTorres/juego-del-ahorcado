ff5607ba953ef6ddbe031be0f2753c22f47c075a
unknown <giselanyelentorres@gmail.com>
2022-09-09 20:59:29 -0300

    cree el canvas y modifique el index y el script

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-const btnIniciar = document.querySelector(".btnIniciar");
+let palabras = ["colores","maestra","recreo","campana"];
+let tablero = document.getElementById("forca").getContext("2d");
+let palabraSecreta = "";
+
+
+function iniciarJuego(){
+    document.getElementById(iniciarJuego).style.display=none;
+
+}
+function escogerPalabraSecreta(){
+    let palabra = palabras[i];
+    let palabra = palabras[Math.floor(Math.random()*palabras.length)];
+    palabraSecreta = palabra;
+
+
+
+}
